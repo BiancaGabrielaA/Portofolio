9b875fd73c0fc92d4c52a116a987861f2fecdaa1
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-05-14 14:41:21 +0300

    Update data in portofolio

--- frontend/src/components/AboutMe.vue
+++ frontend/src/components/AboutMe.vue
@@ -14,13 +14,13 @@
           <!-- Right: Info -->
           <div class="flex-1 text-center md:text-left">
             <p class="text-lg text-gray-300 mb-4 leading-relaxed">
-              Hey! I'm a full-stack developer based in Bucharest, Romania. I graduated from the University of Bucharest in 2024 and specialize in building fast, secure, and scalable web applications.
+              Hey! I'm a full-stack developer based in Bucharest, Romania. I graduated from the University of Bucharest in 2024 and specialize in building fast, secure and scalable web applications.
             </p>
             <p class="text-lg text-gray-300 mb-4 leading-relaxed">
-              I love writing clean code, crafting smooth user experiences, and turning ideas into reality. When I’m not coding, I teach kids how to program in C++ — combining creativity and logic in a way that keeps me inspired.
+              I love writing clean code, crafting smooth user experiences and turning ideas into reality. When I’m not coding, I teach kids how to program in C++ — combining creativity and logic in a way that keeps me inspired.
             </p>
             <p class="text-lg text-gray-300 leading-relaxed">
-              I believe great work comes from balance, so I make time to travel, stay active, and recharge — all while leveling up my skills and taking on new challenges.
+              I believe great work comes from balance, so I make time to travel, stay active and recharge — all while leveling up my skills and taking on new challenges.
             </p>
             <div class="mt-10">
                 <button class="bg-primary-light-pink text-primary-dark-purple px-4 py-2 rounded-md" @click="downloadCV">
