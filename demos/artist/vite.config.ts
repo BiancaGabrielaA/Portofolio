3c8f1c7d0edacf1ad4f7793482e4c098bbca2b05
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-05-14 14:51:54 +0300

    New vite config for artist

--- demos/artist/vite.config.ts
+++ demos/artist/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 3002
+  },
+  preview: {
+    port: 4173,
+    host: true,
+    allowedHosts: ['biancagabriela-dev.com']
   }
 })
