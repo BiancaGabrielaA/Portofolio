947b638d81f26b152510d4e0b16bc60d1dcd33e6
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-05-14 15:10:11 +0300

    For deploy presentation

--- demos/presentation/next.config.ts
+++ demos/presentation/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  basePath: '/demo/presentation',
+  assetPrefix: '/demo/presentation'
 };
 
 export default nextConfig;
