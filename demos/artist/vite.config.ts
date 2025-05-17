b039bcbaa3f2057ecf815a6b53892e1935ae9e62
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-05-17 14:16:24 +0300

    vite config artist file

--- demos/artist/vite.config.ts
+++ demos/artist/vite.config.ts
@@ -8,25 +8,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 3002,
-<<<<<<< HEAD
-    allowedHosts: [
-      'biancagabriela-dev.com',
-      'www.biancagabriela-dev.com',
-      'localhost',
-      '127.0.0.1'
-    ],
-  },
-  preview: {
-    host: true,
-    port: 3002,
-    allowedHosts: [
-      'biancagabriela-dev.com',
-      'www.biancagabriela-dev.com',
-      'localhost',
-      '127.0.0.1'
-    ],
-=======
     allowedHosts: true
->>>>>>> refs/remotes/origin/main
   }
 })
